2debc82b2de87a1fc6a5c8dfc444de71aa73a9db
Guiradi <g.ferreira@unesp.br>
2020-08-17 16:43:57 -0300

    Inicializa repositório

---
+++ T1-Compiladores-Guilherme-Ponsoni-Ferreira.c
@@ -0,0 +1,12 @@
+/*
+ * Trabalho I de Compiladores - Analisador Léxico
+ * Nome: Guilherme Ponsoni Ferreira
+ * RA: 171025367
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    printf("Hello World!");
+}
