11fc35c92e34dab78075077e70b4c2b2aeee130d
Guiradi <g.ferreira@unesp.br>
2020-08-17 16:51:11 -0300

    Adiciona detalhes do compilador utilizado

--- T1-Compiladores-Guilherme-Ponsoni-Ferreira.c
+++ T1-Compiladores-Guilherme-Ponsoni-Ferreira.c
@@ -2,11 +2,14 @@
  * Trabalho I de Compiladores - Analisador Léxico
  * Nome: Guilherme Ponsoni Ferreira
  * RA: 171025367
+ * 
+ * Compilador utilizado: gcc (tdm64-1) 4.9.2
  */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main()
+{
     printf("Hello World!");
 }
